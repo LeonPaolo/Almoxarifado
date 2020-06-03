8cabe73b8240e164810d5a3751c6f64ef3ffd0f6
Leon Paolo <40367631+LeonPaolo@users.noreply.github.com>
2020-06-03 05:37:39 -0300

    Update devolvidos.blade.php

--- resources/views/devolvidos.blade.php
+++ resources/views/devolvidos.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">Almoxafirado</div>
+                <div class="card-header">Itens que foram devolvidos</div>
 
                 <div class="card-body">
                     <table class="table">
